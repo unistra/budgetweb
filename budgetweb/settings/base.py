cc084794c56e1c4135d876c63da471609649814f
Ludovic Hutin <ludovic.hutin@unistra.fr>
2016-05-17 10:26:20 +0200

    Suppresion d'un bug.

--- budgetweb/settings/base.py
+++ budgetweb/settings/base.py
@@ -123,7 +123,7 @@ MEDIA_URL = '/media/'
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
-PROJECT_ROOT ='/var/www/html/budgetweb/budgetweb'
+#PROJECT_ROOT ='/var/www/html/budgetweb/budgetweb'
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/budgetweb/site_media/'
