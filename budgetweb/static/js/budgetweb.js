c6d2ac2824906c501781aa3fff07edda9fffcf79
Ludovic Hutin <ludovic.hutin@unistra.fr>
2016-07-09 21:18:36 +0200

    Correction d'un bug d'affichage sur un glyphicon.

--- budgetweb/static/js/budgetweb.js
+++ budgetweb/static/js/budgetweb.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
         var children = $(this).parent('li.parent_li').find(' > ul > li');
         if (children.is(":visible")) {
             children.hide('fast');
-            $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon glyphicon-plus').removeClass('glyphicon glyphicon-minus');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
         } else {
             children.show('fast');
-            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon glyphicon-minus').removeClass('glyphicon glyphicon-plus');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
             cf = $(this).attr('toto');
             $.ajax({
                          url : window.location.href+"getsubtree/"+cf, // the endpoint,commonly same url
