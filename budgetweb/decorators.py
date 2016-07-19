38580a6af4654071832b2305bad839b7e8d35b2c
Thierry Schlecht <thierry.schlecht@unistra.fr>
2016-07-19 11:20:07 +0200

    Bug dans le décorateur si l'utilisateur n'a aucune permission sur les structures

--- budgetweb/decorators.py
+++ budgetweb/decorators.py
@@ -3,7 +3,7 @@ from functools import wraps
 from django.http import HttpResponseForbidden
 
 from .exceptions import StructureUnauthorizedException
-from .models import PlanFinancement
+from .models import PlanFinancement, StructureAuthorizations
 
 
 def is_authorized_structure(func):
@@ -16,8 +16,12 @@ def is_authorized_structure(func):
             user = request.user
             is_authorized = False
             pfi = PlanFinancement.objects.get(pk=kwargs.get('pfiid'))
-            user_structures = user.structureauthorizations.structures\
-                .filter(is_active=True).values_list('pk', flat=True)
+            user_structures = []
+            try:
+                user_structures = user.structureauthorizations.structures\
+                    .filter(is_active=True).values_list('pk', flat=True)
+            except StructureAuthorizations.DoesNotExist:
+                pass
             is_authorized = user.is_superuser\
                 or pfi.structure.pk in user_structures
             if not is_authorized:
