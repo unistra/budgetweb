cc8779032cb7774884e81c92ec74c171b7685275
Thierry <thierry.schlecht@unistra.fr>
2016-07-23 17:06:51 +0200

    Correction de la fermeture de l'arbre

--- budgetweb/static/js/budgetweb.js
+++ budgetweb/static/js/budgetweb.js
@@ -13,25 +13,25 @@ function initDateTimePicker() {
 $(document).ready(function() {
 	initDateTimePicker();
 
-	$('.tree li:has(ul)').addClass('parent_li').find(' > span.cf').attr('title', 'Ouvrir cette branche');
+	$('.tree li:has(ul)').addClass('parent_li');
 	$('span.cf').unbind('click');
 	$('.tree li.parent_li').on('click', 'span.cf', function (e) {
-		var children = $(this).parent('li.parent_li').find(' > ul > li');
+		var children = $(this).parent('div').parent('li.parent_li').find('> ul li');
 		if (children.is(":visible")) {
 			children.hide('fast');
-			$(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
+			$(this).attr('title', 'Ouvrir cette branche').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
 		} else {
 			children.show('fast');
-			$(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
-			cf = $(this).attr('toto');
+			$(this).attr('title', 'Fermer cette branche').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
+			var cf = $(this).attr('structcode');
 			$.ajax({
 				url : window.location.href + cf + "/", // the endpoint,commonly same url
 				type : "GET", // http method
 				data : { }, // data sent with the post request
 				// handle a successful response
 				success : function(json) {
-                    $("#"+cf).empty().append(json);
-					$('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Ouvrir cette branche');
+                    $("#" + cf).empty().append(json);
+					$('.tree li:has(ul)').addClass('parent_li');
 				},
 				// handle a non-successful response
 				error : function(xhr,errmsg,err) {
