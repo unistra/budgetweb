1830fbc913430a71d900b09d4dbd5da593bf0450
ludovic.hutin <ludovic.hutin@unistra.fr>
2016-09-18 21:54:22 +0200

    Ajout automatique de certain compte dans le groupe DFI.

--- budgetweb/management/commands/perso_import.py
+++ budgetweb/management/commands/perso_import.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.core.management.base import NoArgsCommand
 from django.core.management import call_command
+from django.conf import settings
 
 
 class Command(NoArgsCommand):
@@ -10,3 +11,13 @@ class Command(NoArgsCommand):
         User.objects.create_superuser(
             'ludovic.hutin', 'ludovic.hutin@unistra.fr', '!')
         call_command('initial_import')
+
+        # Ajout de ces utilisateurs au groupe DFI.
+        liste_users = ['b.edel', 'caroline.wolff', 'aurelie.regnier',
+                       'sylvie.siclerc', 'herve.heusser', 'f.idrissi.serh1',
+                       'annie.spinella', 'g.weyrich', 'a.lauffenburger',
+                       'norberti', 'hering']
+        g = Group.objects.get(name=settings.DFI_GROUP_NAME)
+        for u in liste_users:
+            print(u)
+            g.user_set.add(User.objects.get(username=u))
