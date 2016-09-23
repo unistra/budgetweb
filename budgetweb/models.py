bd3437b0585851aa374cab35eac9389bfdbae2d3
tschlecht <thierry.schlecht@gmail.com>
2016-09-23 16:10:51 +0200

    Default period in the empty array of the pluriannuel view

--- budgetweb/models.py
+++ budgetweb/models.py
@@ -233,13 +233,14 @@ class PlanFinancement(models.Model):
         ]
         """
         montants_dict = {'gbcp': ('AE', 'CP', 'AR', 'RE'), 'dc': ('DC',)}
+        default_period = 'BI'
         montant_type = lambda x: [
             k for k, v in montants_dict.items() if x in v][0]
         types = []
         years = self.get_years()
 
         for comptabilite in self.get_total(years=years):
-            compta_types = {k: {} for k in montants_dict.keys()}
+            compta_types = {k: {default_period: {}} for k in montants_dict.keys()}
             for c in comptabilite:
                 fields = [k for k in c.keys() if k.startswith('sum_')]
                 for field in fields:
@@ -267,47 +268,6 @@ class PlanFinancement(models.Model):
                         type_dict[1].get('total', Decimal(0)) + montant
             types.append(compta_types)
         return types
-
-    def get_detail_pfi_by_period(self, totals):
-        # FIXME: docstring
-        montants_dict = {'gbcp': ('AE', 'CP', 'AR', 'RE'), 'dc': ('DC',)}
-        montant_type = lambda x: [
-            k for k, v in montants_dict.items() if x in v][0]
-        details = []
-
-        # Group by year
-        for compta in totals:
-            compta_details = {}
-            for year, year_values in groupby(compta, lambda x: x['annee']):
-                compta_types = {k: [{}, {}] for k in montants_dict.keys()}
-                periodes_set = set()
-                for c in year_values:
-                    periode = c['periodebudget__code']
-                    periodes_set.add(periode)
-                    fields = [k for k in c.keys() if k.startswith('sum_')]
-                    for field in fields:
-                        montant = c[field]
-                        field_name = field.split('_')[-1].upper()
-                        mt = montant_type(field_name)
-                        ct = compta_types[mt]
-                        nature_dict = ct[0].setdefault(
-                            c['enveloppe'], [{}, {}])
-                        type_dict = nature_dict[0].setdefault(
-                            periode, {})
-                        type_dict[field_name] = montant
-
-                        # Total per periode and montant_type
-                        nature_dict[1].setdefault(field_name, Decimal(0))
-                        nature_dict[1][field_name] += montant
-
-                        # Total per enveloppe
-                        total_enveloppe = compta_types[mt][1].setdefault(periode, {})
-                        total_enveloppe[field_name] = total_enveloppe.get(field_name, Decimal(0)) + montant
-
-                # TODO: order periodes_set and global periodes_set for depenses and recettes
-                compta_details[year] = (compta_types, periodes_set)
-            details.append(compta_details)
-        return details
 
 
 class NatureComptableDepense(models.Model):
