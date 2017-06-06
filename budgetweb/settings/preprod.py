46ec0b237997cf0e998b1f2a49a3d095687b86c4
Thierry <thierry.schlecht@unistra.fr>
2017-06-06 09:40:13 +0200

    Missing SIFACWS settings in preprod and prod

--- budgetweb/settings/preprod.py
+++ budgetweb/settings/preprod.py
@@ -46,3 +46,12 @@ SECRET_KEY = '{{ secret_key }}'
 
 DIPSTRAP_VERSION = '{{ dipstrap_version }}'
 DIPSTRAP_STATIC_URL += '%s/' % DIPSTRAP_VERSION
+
+
+#################
+# Interfaces SI #
+#################
+
+SIFACWS_DESC = 'https://rest-api-test.u-strasbg.fr/sifacws/description.json'
+SIFACWS_URL = 'https://sifacws-test.u-strasbg.fr/'
+SIFACWS_APIKEY = environ.get('SIFACWS_APIKEY')
