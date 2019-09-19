121c6ee8bd4ce8f97dde14b6e3f929ae7a83fe7b
Thierry <thierry.schlecht@unistra.fr>
2019-09-19 10:28:14 +0200

    Do not commit debug lines !

--- budgetweb/management/commands/migrate_pluriannuel.py
+++ budgetweb/management/commands/migrate_pluriannuel.py
@@ -70,7 +70,6 @@ class Command(BaseCommand):
                 raise Exception('Erreur : Entrées déjà existantes pour la '
                                 'période BI %s ' % year)
 
-            pfis = PlanFinancement.objects.filter(is_pluriannuel=True)
             depenses = []
             recettes = []
             for pfi in pfis:
