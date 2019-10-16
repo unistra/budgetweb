91e43654bddb5bae5609382b19e41103a40cb399
Ludovic Hutin <ludovic.hutin@unistra.fr>
2019-10-16 10:28:21 +0200

    Remove debug_toolbar...

--- budgetweb/settings/docker.py
+++ budgetweb/settings/docker.py
@@ -1,10 +1,27 @@
+# -*- coding: utf-8 -*-
+
 from os import environ
+from os.path import normpath
+from .base import *
 
-from .dev import *
+#######################
+# Debug configuration #
+#######################
 
-INTERNAL_IPS += ('172.16.240.1', )
-TEMPLATE_DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = True
+
+
+##########################
+# Database configuration #
+##########################
+
+# In your virtualenv, edit the file $VIRTUAL_ENV/bin/postactivate and set
+# properly the environnement variable defined in this file (ie: os.environ[KEY])
+# ex: export DEFAULT_DB_NAME='project_name'
+
+# Default values for default database are :
+# engine : sqlite3
+# name : PROJECT_ROOT_DIR/default.db
 
 DATABASES = {
     'default': {
@@ -16,4 +33,48 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-MIDDLEWARE_CLASSES += ()
+
+#####################
+# Log configuration #
+#####################
+
+LOGGING['handlers']['file']['filename'] = environ.get('LOG_DIR',
+        normpath(join('/tmp', '%s.log' % SITE_NAME)))
+LOGGING['handlers']['file']['level'] = 'DEBUG'
+
+for logger in LOGGING['loggers']:
+    LOGGING['loggers'][logger]['level'] = 'DEBUG'
+
+
+###########################
+# Unit test configuration #
+###########################
+
+INSTALLED_APPS += (
+    'coverage',
+)
+#TEST_RUNNER = 'django_coverage.coverage_runner.CoverageRunner'
+
+############
+# Dipstrap #
+############
+
+DIPSTRAP_VERSION = environ.get('DIPSTRAP_VERSION', 'latest')
+DIPSTRAP_STATIC_URL += '%s/' % DIPSTRAP_VERSION
+
+#################
+# Debug toolbar #
+#################
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+INTERNAL_IPS += ('172.16.240.1', )
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+#################
+# Interfaces SI #
+#################
+
+SIFACWS_DESC = 'https://FIXIT/sifacws/description.json'
+SIFACWS_URL = 'https://FIXIT/'
+SIFACWS_APIKEY = 'FIXIT'
