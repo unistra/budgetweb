5afdbbeae5dfb412e5d1698431cf297f05766f1c
Ludovic Hutin <ludovic.hutin@unistra.fr>
2019-10-16 09:28:44 +0200

    Fix test

--- run_tests.py
+++ run_tests.py
@@ -9,8 +9,9 @@ import django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'budgetweb.settings.unittest'
 django.setup()
 
+from django.conf import settings
 from django.test.runner import DiscoverRunner
-
+from django.test.utils import get_runner
 
 """
 Run tests script
@@ -18,6 +19,7 @@ Run tests script
 
 test_runner = DiscoverRunner(pattern='test*.py', verbosity=2,
                              interactive=True, failfast=False)
-
+TestRunner = get_runner(settings)
+test_runner = TestRunner()
 failures = test_runner.run_tests(['budgetweb'])
 sys.exit(failures)
